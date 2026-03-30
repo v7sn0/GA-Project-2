0a26f919018582f69aa5cac862287fc6db4d81ca
Ali Hasan Salman Hasan <nassy02056@gmail.com>
2026-03-30 13:45:43 +0300

    created router and controller function to the user profile

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,5 +5,5 @@ const userController = require("../controllers/userController.js")
 
 // router.post("/signup", userController.signUpController)
 // router.get("/signin", userController.signInController)
-
+router.get("/:id", userController.showProfile)
 module.exports = router
