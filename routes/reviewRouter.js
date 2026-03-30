73dc48b0a4b69f5d6f5ec93848c3eb1d2b3bc959
Ali Hasan Salman Hasan <nassy02056@gmail.com>
2026-03-30 10:08:30 +0300

    created router for single review

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -3,5 +3,5 @@ const router = express.Router()
 const reviewController = require("../controllers/reviewController.js")
 
 router.post("/addReview", reviewController.addReview)
-
+router.get("/:id", reviewController.showSingleReview)
 module.exports = router
